d71dc0e0a41d4fe081a0b663c1e8ff2da65ea5a9
kustikov <pasha_k1987@mail.ru>
2022-12-23 20:17:54 +0300

    Добавление в словарю основной валюты рубль

--- main.py
+++ main.py
@@ -2,37 +2,34 @@ import requests
 # Реализовать получение курсов по API
 def get_rates_from_api():
     result = requests.get('https://www.cbr-xml-daily.ru/latest.js').json().setdefault('rates')
+    result['RUB'] = 1
     return result
-    
+
 EXCHANGE_RATES = get_rates_from_api()
+print(EXCHANGE_RATES)
 # В выгруженном API словаре нет RUB:1. Нужно ли добавить его туда вручную через  d[key] = value
 
 # Приводим СЛОВАРЬ к строке для отображения списка валют без скобок, удаляем пробелы по краям.
 def rates_for_input():  
     dict_a = EXCHANGE_RATES
     result = ''
-    
     for key in dict_a:
-        result += f' {key}'
-        
+        result += f' {key}'    
     return result.strip()
     
 def main():
     exchange_rates = get_rates_from_api()
+    main_currency = exchange_rates['RUB']
+    print(main_currency)
     rates_input = rates_for_input()
     base_currency = input(f'Введите валюту, которую хотите обменять =>\n{rates_input}\n')
     base_currency = base_currency.upper() #приводим к UPPER если пользователь ввел не тот регистр
-
-    def isBaseCurrency_include_rates_input(base_currency):
-        check_key = base_currency in EXCHANGE_RATES.keys()
-        print(check_key)
-        if check_key:
-            return
-        else:
-            return f'Введена несуществующая валюта {base_currency}'
-    #Функция не возвращает результат в ветви else если введена не та валюта, программа переходит на строки ниже. С чем это может быть связано?
-
-    isBaseCurrency_include_rates_input(base_currency) #Вызываем функцию проверки действительна ли валюта.
+    check_key = base_currency in EXCHANGE_RATES.keys()
+    print(check_key)
+    if check_key:
+            'Валюта верна'
+    else:
+        print(f'Введена несуществующая валюта')
     # Исправить отображение списка валют
     # Проверять, действительную ли валюту ввел пользователь и отрицательные значения
     # Обрезать лишние пробелы при вводе, проверять регистр ввода
@@ -42,8 +39,13 @@ def main():
     # Проверять, действительную ли валюту ввел пользователь и отрицательные значения
     # Обрезать лишние пробелы при вводе, проверять регистр ввода
     currency_amount = float(input('Введите количество валюты\n'))
-
-    cross_course = exchange_rates[base_currency] / exchange_rates[target_currency]
+    #Если одна из выбранных валют: рубль (как основная)
+    if base_currency == 'RUB':
+    	cross_course = exchange_rates[base_currency] * exchange_rates[target_currency]
+    elif target_currency == 'RUB':
+         cross_course = (exchange_rates[target_currency] / exchange_rates[base_currency])
+    else:
+    	cross_course = exchange_rates[base_currency] / exchange_rates[target_currency]
     result = round(cross_course * currency_amount, 2)
     print(f'Вы получите = {result} {target_currency}')  # добавить валюту в вывод  - done
 
